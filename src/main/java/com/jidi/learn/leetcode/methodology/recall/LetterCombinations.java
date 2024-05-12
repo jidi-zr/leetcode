47c9e42872268897ddca6edc700c400bdf73c3fb
jidi <jidi_jidi@163.com>
2024-05-12 22:25:35 +0800

    todo：17. 电话号码的字母组合

---
+++ src/main/java/com/jidi/learn/leetcode/methodology/recall/LetterCombinations.java
@@ -0,0 +1,18 @@
+package com.jidi.learn.leetcode.methodology.recall;
+
+import java.util.List;
+
+/**
+ * 17. 电话号码的字母组合  https://leetcode.cn/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * @author: jidi
+ * @email: jidi_jidi@163.com
+ * @date 2024/5/12
+ */
+public class LetterCombinations {
+
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+
+}
--- src/main/java/com/jidi/learn/leetcode/methodology/recall/ReadMe.txt
+++ src/main/java/com/jidi/learn/leetcode/methodology/recall/ReadMe.txt
@@ -1,2 +1,3 @@
 保存回溯相关题目跟解法
 
+回溯算法力扣题目总结：https://leetcode.cn/problems/letter-combinations-of-a-phone-number/solutions/2566775/dai-ma-sui-xiang-lu-leetcode17dian-hua-h-6hbc/
