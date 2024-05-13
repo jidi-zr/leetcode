43ad3dbfd0da1bc04b63e0042265c216b8751fa5
jidi <jidi_jidi@163.com>
2024-05-13 22:43:58 +0800

    feat：17. 电话号码的字母组合

--- src/main/java/com/jidi/learn/leetcode/methodology/recall/LetterCombinations.java
+++ src/main/java/com/jidi/learn/leetcode/methodology/recall/LetterCombinations.java
@@ -1,5 +1,7 @@
 package com.jidi.learn.leetcode.methodology.recall;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,11 +13,47 @@ import java.util.List;
  */
 public class LetterCombinations {
 
+    String[] letters = {" ", "*", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    List<String> res = new ArrayList<>();
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+
     /**
-     * todo 
+     * 回溯
      */
     public List<String> letterCombinations(String digits) {
-        return null;
+        if (digits == null || digits.length() == 0) {
+            return Collections.emptyList();
+        }
+        // 迭代处理
+        this.backTracking(digits, 0);
+        return res;
+    }
+
+
+    private void backTracking(String digits, int level) {
+        // 层数与字符长度一致，添加到结果集
+        if (level == digits.length()) {
+            res.add(stringBuilder.toString());
+            return;
+        }
+
+        // 获取当前数字代表的字符串
+        String letter = letters[digits.charAt(level) - '0'];
+        for (int i = 0; i < letter.length(); i++) {
+            stringBuilder.append(letter.charAt(i));
+            // 递归调用
+            this.backTracking(digits, level + 1);
+            // 去除最后一位，继续比较
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new LetterCombinations().letterCombinations("23").stream().forEach(System.out::println);
     }
 
 }
